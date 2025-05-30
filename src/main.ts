d47310ca80cf079247643323753887127d1d2a48
ArtHard <1artem1nazarov1@gmail.com>
2025-05-30 13:28:39 +0300

    add .env config settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 4200);
+
+	const configService = app.get(ConfigService);
+
+	await app.listen(configService.get<number>("PORT") || 3000);
 }
 
 void bootstrap();
